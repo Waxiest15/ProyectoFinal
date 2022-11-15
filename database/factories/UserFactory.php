9b16f876b1909f5b4d94683072199263fb829893
Dec4rx <decarwar@gmail.com>
2022-11-14 23:47:19 -0600

    User_Factory
    Cambiar valores en un futuro XD

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,9 +18,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'name' =>  $this->faker->name(),
+            'last_name'=>$this->faker->lastname(),
+            'last_name2'=>$this->faker->lastname(),
+            'age'=>$this->faker->numberBetween(15,80),
+            'email' => $this->faker->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\User::factory(10)->state(new Sequence(
+            ['gender' => 'masculino'],
+            ['gender' => 'femenino'],
+            ['gender' => 'no-binario'],
+            ['gender' => 'mayionisaaa'],
+            ['gender' => 'helicóptero apache no-binario'],
+        ))
+        ->create();
     }
 }
